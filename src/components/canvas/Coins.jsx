a63910ef382aa6fe0e7741e6fdeb23d459fddaff
Ivan Christian <ivanvan0562@gmail.com>
2024-10-15 10:21:03 +0700

    update

--- src/components/canvas/Coins.jsx
+++ src/components/canvas/Coins.jsx
@@ -1,29 +1,36 @@
-import { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Coins = ({ isMobile }) => {
+const Coins = () => {
   const coin = useGLTF("/gold_coin/scene.gltf");
+  const coinRef = useRef();
+
+  useFrame(() => {
+    if (coinRef.current) {
+      coinRef.current.rotation.y += 0.01;
+    }
+  });
 
   return (
-    <mesh>
+    <mesh ref={coinRef} scale={0.016}>
       <ambientLight intensity={0.3} groundColor="black" />
       <ambientLight intensity={0.2} />
       <pointLight intensity={1} position={[0, 5, 5]} />
       <spotLight intensity={5} angle={0.3} penumbra={1} position={[5, 10, 10]} castShadow color="#ffd700" />
       <directionalLight intensity={5} position={[-5, -3, 10]} color="#ffffff" castShadow />
-      <primitive object={coin.scene} scale={0.11} position={[0, -1.8, 0]} rotation={[0, Math.PI + 0.9, 0]} />
+      <primitive object={coin.scene} position={[1, -18, 0]} />
     </mesh>
   );
 };
 
 const CoinsCanvas = () => {
   return (
-    <Canvas frameloop="demand" shadows camera={{ position: [20, 2, 5], fov: 35 }} gl={{ preserveDrawingBuffer: true }}>
+    <Canvas frameloop="always" shadows camera={{ position: [0, 0, 3], fov: 35 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <Coins />
+        <OrbitControls enableZoom={false} />
       </Suspense>
       <Preload all />
     </Canvas>
